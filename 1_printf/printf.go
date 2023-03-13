da378113f5f02dda5000199469ff7406198567ee
Umi-no-Yamaneko <ml00-uymnk@outlook.jp>
2023-03-13 10:04:51 +0900

    ]

---
+++ 1_printf/printf.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	num := 123
+	str := "ABC"
+
+	fmt.Print("num=",num, " str=", str, "¥n")
+	fmt.Println("num=", num, "str =", str)
+	fmt.Printf("num=%d str=%s¥n", num, str)
+}
+
